78133e49b6476c79e9758d9a92f395b78cba8ad3
Max Fowler <maxhfowler@gmail.com>
2019-01-29 00:30:34 +0100

    Upload

--- pd_python/ptest2.py
+++ pd_python/ptest2.py
@@ -6,4 +6,12 @@ ThermalPrinter = adafruit_thermal_printer.get_printer_class(2.68)
 
 printer = ThermalPrinter(uart)
 
-printer.test_page()
+def print_number(number):
+    printer.print('Thank you using Pin Daddy')
+    printer.feed(2)
+
+    printer.print_barcode(str(number), printer.UPC_A)
+
+
+if __name__ == '__main__':
+    print_number(1234)
