4f716ab2dc539ff081f42cb90df4538843f15f19
Max Fowler <maxhfowler@gmail.com>
2019-01-29 19:12:43 +0100

    Upload

--- pd_python/rotary_test.py
+++ pd_python/rotary_test.py
@@ -3,6 +3,31 @@ import time
 import threading
 import random
 
+rot5 = 5
+rot6 = 6
+rot13 = 13
+rot19 = 19
+rot26 = 26
+rot12 = 12
+rot16 = 16
+rot20 = 20
+rot21 = 21
+rots = [
+    rot5, rot6, rot13, rot19, rot26, rot12, rot16, rot20, rot21
+]
+
+
+def get_rot_value():
+    rot_dict = {}
+    for rot in rots:
+        val = GPIO.input(rot)
+        print('{}: {}'.format(rot, val))
+        rot_dict[rot] = val
+
+
+
+
+
 
 if __name__ == '__main__':
     import RPi.GPIO as GPIO
@@ -18,27 +43,13 @@ if __name__ == '__main__':
         GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
         # setup rots
-        rot5 = 5
-        rot6 = 6
-        rot13 = 13
-        rot19 = 19
-        rot26 = 26
-        rot12 = 12
-        rot16 = 16
-        rot20 = 20
-        rot21 = 21
-        rots = [
-            rot5, rot6, rot13, rot19, rot26, rot12, rot16, rot20, rot21
-        ]
         for rot in rots:
             GPIO.setup(rot, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
         while True:
             # waiting for input
             print('++ waiting for input')
-
-            for rot in rots:
-                print('{}: {}'.format(rot, GPIO.input(rot)))
+            rot_val = get_rot_value()
             time.sleep(2)
 
     finally:
