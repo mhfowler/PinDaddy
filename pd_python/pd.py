1e73d41a19401788ee2088a8b0f14ac9e18b3f35
Max Fowler <maxhfowler@gmail.com>
2019-01-29 05:35:10 +0100

    Upload

--- pd_python/pd.py
+++ pd_python/pd.py
@@ -61,10 +61,10 @@ class PD:
     def click(self):
         self.pen_down()
         # then pen half up
-        self.write('G90 G1 Z27 F3600\n')
+        self.write('G90 G1 Z22 F3600\n')
 
     def pen_down(self):
-        self.write('G90 G1 Z29 F3600\n')
+        self.write('G90 G1 Z26 F3600\n')
         grbl_out = self.serial.readline()
 
     def pen_up(self):
